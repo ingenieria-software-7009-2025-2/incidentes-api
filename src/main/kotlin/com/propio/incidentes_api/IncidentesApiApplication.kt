eb8280f25bc579dbde54a97684bfcc248424c517
LuisBerna <luis_berna@ciencias.unam.mx>
2025-03-08 23:12:32 -0600

    chore: add comment

--- src/main/kotlin/com/propio/incidentes_api/IncidentesApiApplication.kt
+++ src/main/kotlin/com/propio/incidentes_api/IncidentesApiApplication.kt
@@ -5,7 +5,7 @@ import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class IncidentesApiApplication
-
+//Fooo
 fun main(args: Array<String>) {
 	runApplication<IncidentesApiApplication>(*args)
 }
